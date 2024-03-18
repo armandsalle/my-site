229c9a8f673a82413590e3f19c331f3606b130bf
armand <armand.salle@gmail.com>
2024-03-18 18:22:13 +0100

    Update site URL in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,7 @@ import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://armand-salle.fr",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   adapter: vercel(),
   output: "static",
