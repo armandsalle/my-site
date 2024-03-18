6cfcd710a6b48ffd3f03224784e83b4289009dc9
armand <armand.salle@gmail.com>
2024-03-18 18:13:13 +0100

    Add @astrojs/vercel package and update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/static"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: "server"
-});
+  adapter: vercel(),
+})
