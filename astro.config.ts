522c5174d15318d729411de5435b440e84e9812b
armand <armand.salle@gmail.com>
2024-04-09 16:13:29 +0200

    Migrate to VPS

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/static"
 import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://armand-salle.fr",
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  adapter: vercel(),
   output: "static",
 })
